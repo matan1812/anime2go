9d794034ea9130a9633aabca511f27799f4084ed
Matan <matan1812.mm@gmail.com>
2020-11-16 19:58:18 +0200

    make it better

---
+++ routes/prods.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const {prodsModel,validProds} = require("../models/prods_model");
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  //req.params, req.query , req.body
+  prodsModel.find()
+  .then(data => {
+    res.json(data);
+
+  })
+});
+
+//localhost:3000/search/?q=fo
+router.get('/search', (req, res, next) => {
+  //req.params, req.query , req.body
+  let searchQ = new RegExp(req.query.q)
+  prodsModel.find({$or:[{name:searchQ},{cat:searchQ}]})
+  .then(data => {
+    res.json(data);
+
+  })
+});
+
+
+router.post("/add",(req,res) => {
+  let validData = validProds(req.body);
+  if(validData.error){
+    return res.status(400).json(validData.error.details);
+  }
+  prodsModel.insertMany([req.body])
+  .then(data => {
+    res.status(201).json(data[0])
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  })
+})
+
+module.exports = router;
+
+
